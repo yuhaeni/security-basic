b0f46819b838a4a1bece1127ea2af4e5f361041f
YuHaeni <haeni311@gmail.com>
2023-10-06 17:56:25 +0900

    유저 controller 생성 회원가입 로직 추가

---
+++ src/main/java/com/example/basic/users/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.basic.users.controller;
+
+import com.example.basic.users.dto.UserSignUpDto;
+import com.example.basic.users.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("sign-up")
+    public String singUp (@RequestBody UserSignUpDto dto) throws Exception {
+        userService.signUp(dto);
+        return "회원가입 성공";
+    }
+
+
+}
