732d1926511255ef7dc0e22eeaf288b5f847fdb8
YuHaeni <haeni311@gmail.com>
2023-10-19 18:09:57 +0900

    토큰 응답 DTO 구현

---
+++ src/main/java/com/example/basic/global/dto/ResponseTokenDto.java
@@ -0,0 +1,16 @@
+package com.example.basic.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponseTokenDto {
+
+    private String token;
+
+}
