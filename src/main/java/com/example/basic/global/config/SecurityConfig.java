4a68522658f5821a943ff26cdb3821df381d0d37
YuHaeni <haeni311@gmail.com>
2023-10-20 17:14:15 +0900

    Spring Security 설정

--- src/main/java/com/example/basic/global/config/SecurityConfig.java
+++ src/main/java/com/example/basic/global/config/SecurityConfig.java
@@ -1,19 +1,50 @@
 package com.example.basic.global.config;
 
+import com.example.basic.global.filter.JwtAuthenticationFilter;
+import com.example.basic.provider.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtTokenProvider jwtTokenProvider;
+
+    // JWT를 사용하기 위해서는 기본적으로 password encoder가 필요
+    // BCryptPasswordEncoder 사용
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                // basic auth 및 csrf 보안을 사용하지 않는다
+                .httpBasic().disable()
+                .csrf().disable()
+                // JWT를 사용하기 때문에 세션을 사용하지 않는다
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                // 해당 API에 대해서는 모든 요청을 허가한다
+                .antMatchers("/sign-up").permitAll()
+                .anyRequest().authenticated()
+                // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행한다.
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider) , UsernamePasswordAuthenticationFilter.class)
+                ;
+        return httpSecurity.build();
     }
 
 
