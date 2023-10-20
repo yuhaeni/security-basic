fdc452353db89988dc6325987b3320863606f2ba
YuHaeni <haeni311@gmail.com>
2023-10-20 17:11:06 +0900

    주석 추가

--- src/main/java/com/example/basic/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/basic/global/filter/JwtAuthenticationFilter.java
@@ -12,12 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
-public class JwtFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     public static final String AUTHORIZATION_HEADER = "Authorization"; // HTTP 요청 헤더에서 JWT 토큰을 가져오는 데 사용되는 헤더의 이름
     private final JwtTokenProvider jwtTokenProvider;
 
-    public JwtFilter(JwtTokenProvider jwtTokenProvider) {
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
@@ -25,14 +25,16 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        // 1. Request Header에서 JWT 토큰 추출
         String token = jwtTokenProvider.resolveToken(request);
 
-        if(jwtTokenProvider.validateToken(token)) {
+        // 2. validateToken으로 토큰 유효성 검사
+        if(!token.isBlank()&&jwtTokenProvider.validateToken(token)) {
+            // 토큰이 유효할 경우, 토큰에서 Authentication 객체를 가지고 와서  SecurityContextHolder에 저장
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-
         filterChain.doFilter(request ,response);
-
     }
+
 }
