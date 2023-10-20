a90eac8ec2d65864248273d8997310e6c9da276d
YuHaeni <haeni311@gmail.com>
2023-10-20 18:07:16 +0900

    커스텀 UserDetailsService 구현

--- src/main/java/com/example/basic/users/dto/RequestUserDto.java
+++ src/main/java/com/example/basic/users/dto/RequestUserDto.java
@@ -1,9 +1,15 @@
 package com.example.basic.users.dto;
 
 import com.example.basic.users.domain.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.FetchType;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -14,12 +20,17 @@ public class RequestUserDto {
     private String name;
     private int age;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
     public User toEntity() {
         return User.builder()
                 .email(email)
                 .password(password)
                 .name(name)
                 .age(age)
+                .roles(roles)
                 .build();
     }
 
