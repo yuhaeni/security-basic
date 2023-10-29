471df413223f4c4468ce4d6bbe5d69d2f366f421
LAPTOP-U1QG2KAH\haeni <haeni311@gmail.com>
2023-10-29 15:49:03 +0900

    주석 추가

--- src/main/java/com/example/basic/global/config/SecurityConfig.java
+++ src/main/java/com/example/basic/global/config/SecurityConfig.java
@@ -36,9 +36,11 @@ public class SecurityConfig {
                 // JWT를 사용하기 때문에 세션을 사용하지 않는다
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                // 요청들에 대한 접근제한을 설정한다
                 .authorizeHttpRequests()
-                // 해당 API에 대해서는 모든 요청을 허가한다
+                // 해당 요청에 대해서는 모든 요청을 허가한다
                 .antMatchers("/sign-up").permitAll()
+                // 나머지 요청에 대해서는 모두 인증되어야 한다
                 .anyRequest().authenticated()
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행한다.
                 .and()
