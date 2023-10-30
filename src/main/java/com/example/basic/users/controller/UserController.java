3bb5c40440d88d1aaffc932ce808dd4e7fd6d8d4
YuHaeni <haeni311@gmail.com>
2023-10-30 18:21:06 +0900

    @Valid 동작 안 하는 오류 해결 - @Validated 어노테이션 추가함

--- src/main/java/com/example/basic/users/controller/UserController.java
+++ src/main/java/com/example/basic/users/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class UserController {
 
     private final UserService userService;
