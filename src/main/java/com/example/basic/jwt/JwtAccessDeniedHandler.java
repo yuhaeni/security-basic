e5c796663ca25836525237b70edd5910847af376
YuHaeni <haeni311@gmail.com>
2023-10-30 10:30:58 +0900

    필요한 권한이 존재하지 않는 경우, 403 Forbidden 에러를 리턴하는 class 생성

---
+++ src/main/java/com/example/basic/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,18 @@
+package com.example.basic.jwt;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        // 필요한 권한이 존재하지 않는 경우, 403 Forbidden 에러 리턴
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
