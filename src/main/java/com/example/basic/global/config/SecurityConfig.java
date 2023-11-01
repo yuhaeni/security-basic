5f800b285573d85e62b979be4fafecd377aeceb9
YuHaeni <haeni311@gmail.com>
2023-11-01 14:44:39 +0900

    로그인 요청 허용

--- src/main/java/com/example/basic/global/config/SecurityConfig.java
+++ src/main/java/com/example/basic/global/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
                 // 요청들에 대한 접근제한을 설정한다
                 .authorizeHttpRequests()
                 // 해당 요청에 대해서는 모든 요청을 허가한다
-                .antMatchers("/sign-up").permitAll()
+                .antMatchers("/sign-up" ,"/login").permitAll()
                 // 나머지 요청에 대해서는 모두 인증되어야 한다
                 .anyRequest().authenticated()
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행한다.
